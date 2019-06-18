647d0ab7d701a0c36564581cc5d099526cec6b82
Sandeep Gunduboyina <sandeep@guggu.me>
2019-06-18 14:47:10 +0530

    Build

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,24 @@
-import resolve from 'rollup-plugin-node-resolve';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import babel from 'rollup-plugin-babel';
-import pkg from './package.json';
+import resolve from "rollup-plugin-node-resolve";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import pkg from "./package.json";
 
 export default {
-  input: 'lib/index.js',
+  input: "lib/index.js",
   output: {
     file: pkg.module,
-    exports: 'named',
-    format: 'cjs'
+    exports: "named",
+    format: "cjs"
   },
   plugins: [
     peerDepsExternal(),
     resolve(),
+    commonjs({
+      include: "node_modules/**"
+    }),
     babel({
-      exclude: 'node_modules/**', // only transpile our source code
+      exclude: "node_modules/**" // only transpile our source code
     })
   ]
 };
